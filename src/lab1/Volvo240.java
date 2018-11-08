ab7b3985795e5f00ff789369026e01782dfda0b5
Albin Johansson <albinoscar99@hotmail.com>
2018-11-08 15:05:17 +0100

    Documentation of the Volvo240 class.

--- src/lab1/Volvo240.java
+++ src/lab1/Volvo240.java
@@ -1,9 +1,17 @@
 package lab1;
 
-import java.awt.*;
+import java.awt.Color;
 
+/**
+ * A class that extends <code>Car</code>, that represents a Volvo 240.
+ *
+ * @see Car
+ */
 public class Volvo240 extends Car {
 
+    /**
+     * Affects the speed factor of the car.
+     */
     private final static double TRIM_FACTOR = 1.25;
 
     public Volvo240() {
@@ -11,6 +19,9 @@ public class Volvo240 extends Car {
         setColor(Color.BLACK);
     }
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public double getSpeedFactor() {
         return getEnginePower() * 0.01 * TRIM_FACTOR;
