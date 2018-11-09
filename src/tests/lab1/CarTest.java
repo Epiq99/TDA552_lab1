323c0a3dbcc54864bffeec4c6b99e1880c278abd
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2018-11-09 13:06:51 +0100

    Update src/tests/lab1/CarTest.java

--- src/tests/lab1/CarTest.java
+++ src/tests/lab1/CarTest.java
@@ -8,7 +8,9 @@ public class CarTest {
 
     @Test
     public void Car() {
-
+        Car car = new Saab95();
+        assertTrue(car.getDirection() == Direction.UP);
+        assertTrue(0 == Double.compare(0.0, car.getCurrentSpeed()));
     }
 
     @Test
