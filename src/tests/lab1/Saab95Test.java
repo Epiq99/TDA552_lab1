31b8da703ae94936c1abb362b8174708e35a2a63
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2018-11-09 11:39:00 +0100

    Saab95 fully tested

--- src/tests/lab1/Saab95Test.java
+++ src/tests/lab1/Saab95Test.java
@@ -1,9 +1,23 @@
 package lab1;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
+import sun.rmi.runtime.NewThreadAction;
+
+import java.awt.*;
 
 public class Saab95Test {
+
+    @Test
+    public void Saab95() {
+        Car saab = new Saab95();
+        assertEquals("Saab95", saab.getModelName());
+        assertEquals(0, Double.compare(125.0, saab.getEnginePower()));
+        assertEquals( 2, saab.getNDoors());
+        assertTrue(Color.RED == saab.getColor());
+        assertFalse(((Saab95) saab).isTurboOn());
+    }
 
     @Test
     public void setTurboOn() {
@@ -21,6 +35,19 @@ public class Saab95Test {
 
     @Test
     public void getSpeedFactor() {
+        Car saab = new Saab95();
+        double turbo = 1;
+        if (((Saab95) saab).isTurboOn()) {
+            turbo = 1.3;
+        }
+        double ans = saab.getEnginePower() * 0.01 * turbo;
+        assertEquals(0, Double.compare(ans, saab.getSpeedFactor()));
+    }
 
+    @Test
+    public void isTurboOn() {
+        Saab95 saab95 = new Saab95();
+        saab95.setTurboOn();
+        assertTrue(true == saab95.isTurboOn());
     }
 }
