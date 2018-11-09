cf17b7390dd95c26c25ce81758ed416dfbca4e08
Albin Johansson <albinoscar99@hotmail.com>
2018-11-09 10:56:44 +0100

    Added test classes for Volvo240 and Saab95.

---
+++ src/tests/lab1/Saab95Test.java
@@ -0,0 +1,22 @@
+package lab1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Saab95Test {
+
+    @Test
+    public void setTurboOn() {
+
+    }
+
+    @Test
+    public void setTurboOff() {
+
+    }
+
+    @Test
+    public void getSpeedFactor() {
+
+    }
+}
---
+++ src/tests/lab1/Volvo240Test.java
@@ -0,0 +1,11 @@
+package lab1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Volvo240Test {
+
+    @Test
+    public void getSpeedFactor() {
+    }
+}
